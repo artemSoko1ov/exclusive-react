228314ee08b2b78ad77fd6451d2c47ed7a5ebb17
Artem Sokolov <the.jelll.2006@gmail.com>
2025-09-07 12:46:29 +0500

    added: layouts and favicon

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
+import '@/styles'
+import Header from "@/layouts/Header"
+import Content from "@/layouts/Content";
+import Footer from "@/layouts/Footer";
+
 function App() {
 
   return (
     <>
-      РАБОТАЕТ
+      <Header />
+      <Content />
+      <Footer />
     </>
   )
 }
